6b886956de3b930978a9c50acfe78601a0ec9448
Howard Yang <yanghoward1@gmail.com>
2024-01-20 13:18:48 -0500

    Time Controller Scene reset

---
+++ Loop/Assets/Scripts/TimeController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimeController : MonoBehaviour, Timer
+{
+    public TextMeshProUGUI timerText;
+    public float time = 30.0f;
+
+    private bool resetting;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (resetting)
+        {
+            return;
+        }
+        time -= Time.deltaTime;
+        if (time <= 0)
+        {
+            time = 0f;
+            resetting = true;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        var timeString = time.ToString("N2").Split(".");
+
+        timerText.SetText($"{timeString[0]}:{timeString[1]}");
+    }
+
+    public void SetTime(float newTime)
+    {
+        time = newTime;
+    }
+
+    public float GetTime()
+    {
+        return time;
+    }
+}
